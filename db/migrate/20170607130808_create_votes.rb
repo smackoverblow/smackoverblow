a160a403767baeac2ec7fda7880d95009da1d048
sviderock <myroslav.sviderok@gmail.com>
2017-06-07 16:12:39 +0300

    create vote migration

---
+++ db/migrate/20170607130808_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :votes do |t|
+      t.integer :vote_type
+      t.integer :voter_id
+
+      t.timestamps
+    end
+    add_index :votes, :voter_id
+    add_index :votes, [:voter_id], unique: true
+  end
+end

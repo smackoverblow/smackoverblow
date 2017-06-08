a42158b9e50ed53f037501556ff09dfbac82b50d
sviderock <myroslav.sviderok@gmail.com>
2017-06-08 16:16:20 +0300

    add associations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,34 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :answers, class_name: 'Post', foreign_key: :answered_id
   has_many :accepted_answer, class_name: 'Post', foreign_key: :accepted_id
+  has_many :votes_relationships,  class_name: 'Vote',
+                                  foreign_key: :post_id,
+                                  dependent: :destroy
+  has_many :votes_up,     -> { where(votes: { vote_type: 1 }) },
+                          through: :votes_relationships,
+                          source: :voter
+  has_many :votes_down,   -> { where(votes: { vote_type: 0 }) },
+                          through: :votes_relationships,
+                          source: :voter
 
-  default_scope -> {order(created_at: :desc)}
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true
-  validates :content, presence: true, length: {minimum: 5}
+  validates :content, presence: true, length: { minimum: 5 }
+
+  def delete_vote_up(other_user)
+    votes_up.delete(other_user)
+  end
+
+  def delete_vote_down(other_user)
+    votes_down.delete(other_user)
+  end
+
+  def voted_up?(other_user)
+    votes_up.include?(other_user)
+  end
+
+  def voted_down?(other_user)
+    votes_down.include?(other_user)
+  end
 end

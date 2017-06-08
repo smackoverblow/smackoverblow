1ed54d9a7a8605a29030e9b1e61aef419a8f197d
sviderock <myroslav.sviderok@gmail.com>
2017-06-08 16:16:00 +0300

    create vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :voter, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
   validates :voter_id, presence: true
+  validates :post_id, presence: true
+  validates :vote_type, presence: true, inclusion: { in: [0, 1] }
 end

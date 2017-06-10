e19382a3799e1819ee0cf5c143f7abfe865163b5
sviderock <myroslav.sviderok@gmail.com>
2017-06-11 01:36:16 +0300

    add user feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    @posts = @user.question_feed
+    redirect_to root_url && return unless @user.activated?
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -68,6 +68,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  def question_feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id).order(created_at: :asc)
+  end
 
   def follow(other_user)
     following << other_user

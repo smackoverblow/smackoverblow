3932c2c1ff5a3e2ee48a5fefa7b414ef755d8bd9
sviderock <myroslav.sviderok@gmail.com>
2017-06-11 01:37:46 +0300

    remove unnecessary spaces in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,15 @@
 Rails.application.routes.draw do
-  get 'votes/create'
-
-  get 'votes/destroy'
-
-  get 'posts/create'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
   root    'static_pages#home'
-  get     '/contacts',  to: 'static_pages#contacts'
-  get     '/about',     to: 'static_pages#about'
-  get     '/help',      to: 'static_pages#help'
+  get 'votes/create'
+  get 'votes/destroy'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   get     '/signup',    to: 'users#new'
   post    '/signup',    to: 'users#create'
-  # patch   '/users/:id/edit',       to: 'users#update'
   get     '/login',     to: 'sessions#new'
   post    '/login',     to: 'sessions#create'
   delete  '/logout',    to: 'sessions#destroy'
-  # post    '/vote_up',   to: 'votes#create'
-  # post    '/vote_down', to: 'votes#create'
   resources :users do
     member do
       get :following, :followers, :questions
